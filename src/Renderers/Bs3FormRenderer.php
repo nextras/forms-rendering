f698116dcf1423f9f453eeca0ec5f3884aa63845
Jan Drábek <jan@drabek.cz>
2024-08-02 14:23:36 +0200

    Fix uninitialized access

--- src/Renderers/Bs3FormRenderer.php
+++ src/Renderers/Bs3FormRenderer.php
@@ -44,7 +44,7 @@ class Bs3FormRenderer extends DefaultFormRenderer
 
 	public function render(Form $form, string $mode = null): string
 	{
-		if ($this->form !== $form) {
+		if (!isset($this->form) || $this->form !== $form) {
 			$this->controlsInit = false;
 		}
 
